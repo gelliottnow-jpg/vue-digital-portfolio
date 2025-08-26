23eb4d8ade8cd5ffd23c8529997ad27587348e77
gelliottnow-jpg <gelliottnow@gmail.com>
2025-08-26 14:13:35 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Vue Digital Portfolio</title>
+      </head>
+      <body className="font-sans">{children}</body>
+    </html>
+  )
+}
